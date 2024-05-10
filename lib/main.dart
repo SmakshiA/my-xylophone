195768e5a562666701dcf5762b5344b4daf1e287
smakshi <smakshina1306@gmail.com>
2024-05-10 15:33:15 +0530

    App working

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(
-        elevation: 15,
-        shadowColor: Colors.black,
-        title: Text(
-          'Xylophone',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 32.0,
+  // nouns.take(50).forEach(print);
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 15,
+          shadowColor: Colors.black,
+          title: Text(
+            'Xylophone',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 32.0,
+            ),
           ),
+          backgroundColor: Colors.black,
         ),
-        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: MyApp(),
+        ),
+        backgroundColor: Colors.white,
       ),
-      body: SafeArea(
-        child: MyApp(),
-      ),
-      backgroundColor: Colors.white,
     ),
-  ),);
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -35,6 +40,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final player = AudioPlayer();
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -42,42 +48,100 @@ class _MyAppState extends State<MyApp> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
-            child: Container(
-              color: Colors.red,
+            child: IconButton(
+              padding: EdgeInsets.all(0.0),
+              icon: Container(
+                color: Colors.red,
+              ),
+              onPressed: () {
+                playSound("1");
+              },
             ),
           ),
           Expanded(
-            child: Container(
-              color: Colors.orange,
+            child: IconButton(
+              padding: EdgeInsets.all(0.0),
+              icon: Container(
+                color: Colors.orange,
+              ),
+              onPressed: () {
+                // setState(() {
+                playSound("2");
+                // });
+              },
             ),
           ),
           Expanded(
-            child: Container(
-              color: Colors.yellow,
+            child: IconButton(
+              padding: EdgeInsets.all(0.0),
+              icon: Container(
+                color: Colors.yellow,
+              ),
+              onPressed: () {
+                // setState(() {
+                playSound("3");
+                // });
+              },
             ),
           ),
           Expanded(
-            child: Container(
-              color: Colors.green,
+            child: IconButton(
+              padding: EdgeInsets.all(0.0),
+              icon: Container(
+                color: Colors.green,
+              ),
+              onPressed: () {
+                // setState(() {
+                playSound("4");
+                // });
+              },
             ),
           ),
           Expanded(
-            child: Container(
-              color: Colors.teal,
+            child: IconButton(
+              padding: EdgeInsets.all(0.0),
+              icon: Container(
+                color: Colors.teal,
+              ),
+              onPressed: () {
+                // setState(() {
+                playSound("5");
+                // });
+              },
             ),
           ),
           Expanded(
-            child: Container(
-              color: Colors.blue,
+            child: IconButton(
+              padding: EdgeInsets.all(0.0),
+              icon: Container(
+                color: Colors.blue,
+              ),
+              onPressed: () {
+                // setState(() {
+                playSound("6");
+                // });
+              },
             ),
           ),
           Expanded(
-            child: Container(
-              color: Colors.purple,
+            child: IconButton(
+              padding: EdgeInsets.all(0.0),
+              icon: Container(
+                color: Colors.purple,
+              ),
+              onPressed: () {
+                // setState(() {
+                playSound("7");
+                // });
+              },
             ),
           )
         ],
       ),
     );
+  }
+
+  Future<void> playSound(var num) async {
+    await player.play(AssetSource("note$num.wav"));
   }
 }
